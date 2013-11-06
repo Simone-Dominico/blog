b6952e9cf74102262e40a61ff90a95620bf8c900
Diego Marczal <dmarczal@gmail.com>
2013-11-06 01:45:57 -0200

    changes for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,3 @@
-#require "bundler/capistrano"
-
-#set :stages, %w(staging production)
-#set :default_stage, "production"
 set :rails_env, 'production'
 
 set :rvm_type, :user
@@ -10,24 +6,17 @@ set :rvm_ruby_version, '2.0.0'
 set :application, "blog"
 set :user, 'deployer'
 set :deploy_via, :remote_cache
-#set :use_sudo, false
 
 set :scm, "git"
 set :repo_url, "/home/deployer/repos/#{fetch(:application)}.git"
 set :branch, "master"
 
-#set :shared_path, "/home/deployer/apps/#{fetch(:application)}/shared"
-
 set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
-#default_run_options[:pty] = true
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_files, %w{config/database.yml}
-#default_run_options[:pty] = true
-#ssh_options[:forward_agent] = true
+set :normalize_asset_timestamps, false
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
@@ -52,61 +41,11 @@ namespace :deploy do
   end
   after "deploy:updated", "deploy:setup_config"
 
-  #task :symlink_config, roles: :app do
-  #  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  #end
-  #after "deploy:finalize_update", "deploy:symlink_config"
-
-  #desc "Make sure local git is in sync with remote."
-  #task :check_revision do
-  #  on roles(:app) do
-  #    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-  #      puts "WARNING: HEAD is not the same as origin/master"
-  #      puts "Run `git push` to sync changes."
-  #      exit
-  #    end
-  #  end
-  #end
-  #before "deploy", "deploy:check_revision"
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+       within release_path do
+         execute :rake, 'cache:clear'
+       end
+    end
+  end
 end
-
-# set :application, 'my_app_name'
-# set :repo_url, 'git@example.com:me/my_repo.git'
-
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
-
-#namespace :deploy do
-#
-#  desc 'Restart application'
-#  task :restart do
-#    on roles(:app), in: :sequence, wait: 5 do
-#      # Your restart mechanism here, for example:
-#      # execute :touch, release_path.join('tmp/restart.txt')
-#    end
-#  end
-#
-#  after :restart, :clear_cache do
-#    on roles(:web), in: :groups, limit: 3, wait: 10 do
-#      # Here we can do anything such as:
-#      # within release_path do
-#      #   execute :rake, 'cache:clear'
-#      # end
-#    end
-#  end
-#
-#  after :finishing, 'deploy:cleanup'
-#
-#end
